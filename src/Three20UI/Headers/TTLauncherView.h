d5a4ce72249549d7778f027294a63a1fbf6c5bc8
Christian Lumper <lumper@student.tugraz.at>
2011-05-31 10:25:15 +0200

    -) made startDraggingButton method from TTLauncherView public

--- src/Three20UI/Headers/TTLauncherView.h
+++ src/Three20UI/Headers/TTLauncherView.h
@@ -83,6 +83,7 @@
 
 - (void)endEditing;
 
+- (void)startDraggingButton:(TTLauncherButton*)button withEvent:(UIEvent*)event;
 /**
  * Dims the launcher view except for a transparent circle around the given item. The given text
  * will also be shown center-aligned below or above the circle, as appropriate. The item can be
